ca0b5ed9ab77f48d0f531462c7d3587e29e6225a
gagankumar7 <gaghankumar321@gmail.com>
2025-10-30 23:34:27 +0530

    Update var.tf

--- var.tf
+++ var.tf
@@ -1,3 +1,8 @@
+variable "aws_region" {
+  description = "AWS region where resources will be created"
+  type        = string
+}
+
 variable "instance_type" {
   type = string
   default = "t2.micro"
